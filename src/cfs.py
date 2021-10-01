c7a86da7b7e0a9be9a440b6d63ed18ce5d570086
Ramonmurilo <ramonmurilo@id.uff.br>
2021-09-30 23:03:41 -0300

    IMPLEMENTAÇÃO: Estruturação inicial, download e recorte da chuva observada MERGE e prevista do CFSv2

--- src/cfs.py
+++ src/cfs.py
@@ -3,6 +3,7 @@ import pendulum
 import sys
 import os
 from pathlib import Path
+from cdo import Cdo
 
 def busca_ultimo_dado_disponivel(data_requerida:pendulum.date, url_base:str) -> tuple[pendulum.date,str] :
     """Verifica qual o último dia e horário de rodada disponíveis no NOAA
@@ -36,8 +37,54 @@ def busca_ultimo_dado_disponivel(data_requerida:pendulum.date, url_base:str) -> 
         data = data.subtract(days=1)
 
 
+def baixa_mensal(data_string:str=None, hoje: bool=False, output:str='') -> str:
+    """Baixa arquivo de previsão mensal do modelo CFSv2 gerado com discretização mensal.
+    Caso a data requisitada não esteja disponível, pega a anterior.
 
-def baixa_mensal(data_requerida:str=None,hoje:bool=False,output:str=os.getcwd()) -> str:
+    Args:
+        data_string (str, optional): [DD-MM-YYYY] data requerida dos dados. Defaults to None.
+        hoje (bool, optional): Habilita ou não a data atual. Defaults to False.
+        output (str, optional): Pasta de destino. Defaults to ''- pasta do script.
+
+    Returns:
+        str: Nome do arquivo netcdf4.
+    """
+    cdo = Cdo()
+    
+    data_requerida = pendulum.now('America/Sao_Paulo') if hoje else pendulum.from_format(data_string, 'DD-MM-YYYY')
+    
+    url_base = 'https://ftp.cpc.ncep.noaa.gov/International/nmme/binary_monthly/'
+    arquivo = f'{data_requerida.format("MMM")}IC_cfsv2_precip_anom_stdanom'
+    
+    for terminacao in ('ctl','dat'):
+    
+        url=f'{url_base}/{arquivo}.{terminacao}'
+
+        resp=requests.get(url)
+
+        if resp.status_code == 200:
+
+            resp=requests.get(url).content
+            nome_arquivo = f'{arquivo}.{terminacao}'
+            diretorio = Path(output, nome_arquivo)
+
+            with open(diretorio, "wb") as arquivo_:
+                arquivo_.write(resp)
+                print(f"{nome_arquivo} [ok]")
+
+        else :
+            print('Erro 404. Após verificado a disponibilidade do arquivo, ainda houve erro no download')
+            print('Será baixado o dado do mês anterior')
+            mes_anterior = data_requerida.subtract(months=1).format('DD-MM-YYYY')
+            baixa_mensal2(data_string=mes_anterior)
+            
+    filename_nc = cdo.import_binary(input=f'{output}/{arquivo}.ctl', output=f'{output}/{arquivo}.nc', options='-f nc4')
+    print(f"{filename_nc} [ok]")
+
+    return filename_nc
+
+
+def baixa_mensal2(data_requerida:str=None,hoje:bool=False,output:str=os.getcwd()) -> str:
     """Baixa 7 leads da previsão mensal com atualização diária do modelo cfs.
 
     Args:
@@ -79,3 +126,5 @@ def baixa_mensal(data_requerida:str=None,hoje:bool=False,output:str=os.getcwd())
             sys.exit('Erro 404. Após verificado a disponibilidade do arquivo, ainda houve erro no download')
     
     return 'arquivos baixados'
+
+
--- src/recorte_chuva.py
+++ src/recorte_chuva.py
@@ -1,17 +1,65 @@
-import rioxarray
-import xarray as xr
-import geopandas as gpd
+#import rioxarray
+from shapely.geometry.geo import shape
+#import xarray as xr
+#import geopandas as gpd
+from xarray.core import dataset
+import sys
 
-def preparar_para_recorte(dataset, crs="epsg:4326", xdim="lon", ydim="lat"):
+def preparar_para_recorte_grib(dataset:dataset, crs="epsg:4326", xdim="longitude", ydim="latitude") -> dataset:
+    """Trata coordenadas e dados para recorte | funcional para arquivos grib somente.
+
+    Args:
+        dataset (dataset): Dataset com a variável de interesse selecionada.
+        crs (str, optional): Projeção cartográfica. Defaults to "epsg:4326".
+        xdim (str, optional): Nome da dimensão x. Defaults to "longitude".
+        ydim (str, optional): Nome da dimensão y. Defaults to "latitude".
+
+    Returns:
+        dataset: DataSet pré-cortado
+    """
+    dataset = dataset.assign_coords(longitude=(((dataset.longitude + 180) % 360) - 180)).sortby(xdim)
+    dataset = dataset.rio.set_spatial_dims(x_dim=xdim, y_dim=ydim) 
+    dataset = dataset.rio.write_crs(crs)
+    return dataset
+
+def preparar_para_recorte_nc(dataset:dataset, crs="epsg:4326", xdim="lon", ydim="lat") -> dataset:
+    """Trata coordenadas e dados para recorte | funcional para arquivos netcdf4 somente.
+
+    Args:
+        dataset (dataset): Dataset com a variável de interesse selecionada.
+        crs (str, optional): Projeção cartográfica. Defaults to "epsg:4326".
+        xdim (str, optional): Nome da dimensão x. Defaults to "lon".
+        ydim (str, optional): Nome da dimensão y. Defaults to "lat".
+
+    Returns:
+        dataset: DataSet pré-cortado
+    """
     dataset = dataset.assign_coords(lon=(((dataset.lon + 180) % 360) - 180)).sortby(xdim)
     dataset = dataset.rio.set_spatial_dims(x_dim=xdim, y_dim=ydim) 
     dataset = dataset.rio.write_crs(crs)
     return dataset
 
 
-shp_bacia = gpd.read_file("grande.shp").set_index("Name")
+def main(dados: dataset, contorno:shape, tipo:str='grib') -> dataset:
+    """Recorta dados de chuva com base em um shapefile de bacia.
+    Esta função foi criada para trabalhar com shapefiles disponibilizados por Lis Andrade 
+    em seu repositório do LAMMOC | outros arquivos podem precisar de alterações no código.
 
-# leitura, preparação e recorte dos dados
-dados = xr.open_dataset("precip.mon.mean.nc")
-dados_preparados = preparar_para_recorte(dados.precip)
-dados_recortados = dados_preparados.rio.clip([shp_bacia["geometry"]], "epsg:4326")
+    Args:
+        dados (dataset): Arquivo com a variável já selecionada. Obs.: forneça já na unidade convertida, caso necessário.
+        contorno (shape): Contorno utilizado para recorte
+        tipo (str, optional): aceita 'grib' ou 'nc'. Defaults to 'grib'.
+
+    Returns:
+        dataset: Dados recortados
+    """
+    if tipo == 'grib':
+        dados_preparados = preparar_para_recorte_grib(dados)
+    elif tipo == 'nc':
+        dados_preparados = preparar_para_recorte_nc(dados)
+    else:
+        sys.exit('variável "tipo" incompatível. Selecione {grib ou nc}')
+
+    dados_recortados = dados_preparados.rio.clip([contorno], "epsg:4326")
+    
+    return dados_recortados
